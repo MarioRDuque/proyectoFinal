33bfb8702931f6bab57e3faf23c38018b1592b5c
Mario Mario <marioramos083@gmail.com>
2020-06-25 23:53:21 -0500

    pagina

--- js/controllers.js
+++ js/controllers.js
@@ -10,17 +10,8 @@ angular.module('LibroApp.controllers', []).
       { nombre: "Gestor de Ventas", abreviatura: "GV", autor: "Manuel Manrique" },
       { nombre: "Gestión de Redes", abreviatura: "GR", autor: "Ignacio Chavez" }
     ];
-    $scope.searchFilter = function (driver) {
-      var re = new RegExp($scope.nameFilter, 'i');
-      return !$scope.nameFilter || re.test(driver.Driver.givenName) || re.test(driver.Driver.familyName);
-    };
 
     libroService.listar().then(function (response) {
       console.log(response)
-    });
-
-    libroService.getDrivers().success(function (response) {
-      //Digging into the response to get the relevant data
-      $scope.driversList = response.MRData.StandingsTable.StandingsLists[0].DriverStandings;
     });
   })
--- js/services.js
+++ js/services.js
@@ -2,28 +2,6 @@ angular.module('LibroApp.services', [])
   .factory('libroService', function ($http) {
 
     var libroServicio = {};
-
-    libroServicio.getDrivers = function () {
-      return $http({
-        method: 'JSONP',
-        url: 'http://ergast.com/api/f1/2013/driverStandings.json?callback=JSON_CALLBACK'
-      });
-    }
-
-    libroServicio.getDriverDetails = function (id) {
-      return $http({
-        method: 'JSONP',
-        url: 'http://ergast.com/api/f1/2013/drivers/' + id + '/driverStandings.json?callback=JSON_CALLBACK'
-      });
-    }
-
-    libroServicio.getDriverRaces = function (id) {
-      return $http({
-        method: 'JSONP',
-        url: 'http://ergast.com/api/f1/2013/drivers/' + id + '/results.json?callback=JSON_CALLBACK'
-      });
-    }
-
     libroServicio.listar = function () {
       return $http({
         method: 'GET',
