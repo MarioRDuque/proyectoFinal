c5285431c0b93dfed3243521f5a7a68b326dae7a
Mario Mario <marioramos083@gmail.com>
2020-06-27 13:15:41 -0500

    tests

--- js/controllers.js
+++ js/controllers.js
@@ -50,27 +50,43 @@ angular.module('LibroApp.controllers', []).
     }
 
     $scope.guardar = function (formulario) {
-      if (!formulario.$valid) {
-        $scope.mensaje = "Existen errores en el formulario";
-        $scope.eliminarMensaje();
-      } else {
-        $scope.cargando = true;
-        libroService.guardar($scope.libro).then(function (response) {
-          if (response && response.extraInfo) {
-            $scope.accion = 'Listar';
-            $scope.mensaje = response.operacionMensaje;
-            $scope.libros.push(response.extraInfo);
-          } else {
-            $scope.mensaje = (response && response.operacionMensaje) || "Error desconocido.";
-          }
-          $scope.cargando = false;
+      if ($scope.validarCampos()) {
+        if (!formulario.$valid) {
+          $scope.mensaje = "Existen errores en el formulario";
           $scope.eliminarMensaje();
-        }).catch(function (err) {
-          $scope.cargando = false;
-          $scope.mensaje = err && err.message;
-          $scope.eliminarMensaje();
-        });
+        } else {
+          $scope.cargando = true;
+          libroService.guardar($scope.libro).then(function (response) {
+            if (response && response.extraInfo) {
+              $scope.accion = 'Listar';
+              $scope.mensaje = response.operacionMensaje;
+              $scope.libros.push(response.extraInfo);
+            } else {
+              $scope.mensaje = (response && response.operacionMensaje) || "Error desconocido.";
+            }
+            $scope.cargando = false;
+            $scope.eliminarMensaje();
+          }).catch(function (err) {
+            $scope.cargando = false;
+            $scope.mensaje = err && err.message;
+            $scope.eliminarMensaje();
+          });
+        }
       }
+    }
+
+    $scope.validarCampos = function () {
+      if ($scope.libro.nombre && $scope.libro.nombre.length > 50) {
+        $scope.mensaje = "El nombre del libro debe tener menos de 50 caracteres.";
+        $scope.eliminarMensaje();
+        return false;
+      }
+      if ($scope.libro.nombre && $scope.libro.abreviatura.length > 10) {
+        $scope.mensaje = "El nombre del libro debe tener menos de 10 caracteres.";
+        $scope.eliminarMensaje();
+        return false;
+      }
+      return true;
     }
 
     $scope.guardarEdicion = function (formulario) {
